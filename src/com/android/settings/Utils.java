7e59dc79cfd5382f002d66ecbe8360c33b588794
MrBaNkS <banksmi09@gmail.com>
2014-06-26 06:38:09 -0500

    Note to self: Don't forget important imports :I

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -25,6 +25,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
