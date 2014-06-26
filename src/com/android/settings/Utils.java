605139db595fafffbc9ace1367d0b1b68091b419
MrBaNkS <banksmi09@gmail.com>
2014-06-26 05:57:23 -0500

    Settings: Add isPackageInstalled to Utils.java

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -603,4 +603,20 @@ public class Utils {
         return ((UserManager) context.getSystemService(Context.USER_SERVICE))
                 .getUsers().size() > 1;
     }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        if (pkg == null) {
+            return false;
+        }
+        try {
+            PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+            if (!pi.applicationInfo.enabled) {
+                return false;
+            } else {
+                return true;
+            }
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
 }
