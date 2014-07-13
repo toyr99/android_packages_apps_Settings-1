wanginator/android_packages_apps_settings

Settings:Statusbar notication count (2/2)
Change-Id: I560e6484bb203bbf1f7fc51a7bf5a5987d11e990

Conflicts:
	res/values/gummy_strings.xml
	res/xml/status_bar_settings.xml
	src/com/android/settings/gummy/StatusBar.java

Conflicts:
	res/values/ras_strings.xml

src/com/android/settings/rascarlo/StatusBarSettings.java
@@ -57,12 +57,14 @@ private static final String STATUS_BAR_NETWORK_ACTIVITY = "status_bar_network_ac
     private CheckBoxPreference mStatusBarClock;
     // Quick Settings
     private ListPreference mQuickPulldown;
+    // Notification Count
+    private static final String STATUSBAR_NOTIF_COUNT = "status_bar_notif_count";
     // Network Traffic
     private ListPreference mNetTrafficState;
     private ListPreference mNetTrafficUnit;
     private ListPreference mNetTrafficPeriod;
     private CheckBoxPreference mStatusBarNetworkActivity;
-
+    private CheckBoxPreference mStatusBarNotifCount;
     private int mNetTrafficVal;
     private int MASK_UP;
     private int MASK_DOWN;
@@ -112,6 +114,10 @@ private static final String STATUS_BAR_NETWORK_ACTIVITY = "status_bar_network_ac
                     Settings.System.STATUS_BAR_BATTERY, 0);
             mStatusBarBattery.setValue(String.valueOf(batteryStyleValue));
             mStatusBarBattery.setSummary(mStatusBarBattery.getEntry());
+
+	    // Notification Count
+ 	    mStatusBarNotifCount = (CheckBoxPreference) findPreference(STATUSBAR_NOTIF_COUNT);
+            mStatusBarNotifCount.setOnPreferenceChangeListener(this);
 
             // Network Traffic
             mNetTrafficState = (ListPreference) getPreferenceScreen().findPreference(NETWORK_TRAFFIC_STATE);
@@ -210,6 +216,11 @@ private static final String STATUS_BAR_NETWORK_ACTIVITY = "status_bar_network_ac
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.STATUS_BAR_AM_PM, statusBarAmPm);
             mStatusBarAmPm.setSummary(mStatusBarAmPm.getEntries()[indexAmPm]);
+            return true;
+
+	} else if (preference == mStatusBarNotifCount) {
+            Settings.System.putInt(getActivity().getContentResolver(), Settings.System.STATUSBAR_NOTIF_COUNT,
+                    ((CheckBoxPreference)preference).isChecked() ? 0 : 1);
             return true;
 
         } else if (preference == mNetTrafficState) {

toyr99/android_packages_apps_Settings-1

	src/com/android/settings/rascarlo/StatusBarSettings.java

src/com/android/settings/simpleaosp/StatusBarSettings.java
@@ -22,11 +22,14 @@ OnPreferenceChangeListener {
     private static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
     // Double-tap to sleep
     private static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+    // Notification Count
+    private static final String STATUSBAR_NOTIF_COUNT = "status_bar_notif_count";
 
     private ListPreference mQuickPulldown;
     private CheckBoxPreference mStatusBarBrightnessControl;
     // Double-tap to sleep
     private CheckBoxPreference mStatusBarDoubleTapSleepGesture;
+    private CheckBoxPreference mStatusBarNotifCount;
 
 
     @Override
@@ -34,6 +37,10 @@ OnPreferenceChangeListener {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.status_bar_settings);
+
+ 	    // Notification Count
+ 	    mStatusBarNotifCount = (CheckBoxPreference) findPreference(STATUSBAR_NOTIF_COUNT);
+            mStatusBarNotifCount.setOnPreferenceChangeListener(this);
 
  	    // Status bar double-tap to sleep
             mStatusBarDoubleTapSleepGesture = (CheckBoxPreference) getPreferenceScreen().findPreference(DOUBLE_TAP_SLEEP_GESTURE);
@@ -70,14 +77,17 @@ OnPreferenceChangeListener {
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (preference == mQuickPulldown) {
+	if (preference == mStatusBarNotifCount) {
+            Settings.System.putInt(getActivity().getContentResolver(), Settings.System.STATUSBAR_NOTIF_COUNT,
+                    ((CheckBoxPreference)preference).isChecked() ? 0 : 1);
+            return true;
+        } else if (preference == mQuickPulldown) {
             int quickPulldownValue = Integer.valueOf((String) objValue);
             int quickPulldownIndex = mQuickPulldown.findIndexOfValue((String) objValue);
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.QS_QUICK_PULLDOWN, quickPulldownValue);
             mQuickPulldown.setSummary(mQuickPulldown.getEntries()[quickPulldownIndex]);
             return true;
-
         }
         return false;
     }