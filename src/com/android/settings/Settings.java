d49f4258d8240524888b9b1f5977e159b9f629c3
MrBaNkS <banksmi09@gmail.com>
2014-07-19 17:56:29 -0500

    Let's go back to a more traditional Settings Layout.
    Squashed commit of the following:
    
    commit 06c96b20ed661042f61d6d067205851da94db5d6
    Author: MrBaNkS <banksmi09@gmail.com>
    Date:   Sat Jul 19 17:55:33 2014 -0500
    
        Revert "Move mobile settings to settings headers"
    
        This reverts commit 82abf3b4d6d357e7541e4c98a6c58d30973d8d15.
    
    commit 4b72cff50fddf442e716a70f795ef8dc57ec32f1
    Author: MrBaNkS <banksmi09@gmail.com>
    Date:   Sat Jul 19 17:54:59 2014 -0500
    
        Revert "Upgrade mobilenetwork icon"
    
        This reverts commit c95418f456cacb34d9d40f09b41a52044ddd95f2.
    
    commit 0fd544810cdd0a356e2cb3cd72095bd7917bfe02
    Author: MrBaNkS <banksmi09@gmail.com>
    Date:   Sat Jul 19 17:54:31 2014 -0500
    
        Revert "settings: Enable location mode header switch"
    
        This reverts commit 27fbc1b680705ccf00a64ff08e68f20eee82fd6c.
    
    commit eb3fa5973ffddc5cc0cfdf385bb586d1ee226b88
    Author: MrBaNkS <banksmi09@gmail.com>
    Date:   Sat Jul 19 17:54:12 2014 -0500
    
        Revert "Settings : Switch state fix"
    
        This reverts commit b0e9df97c2515dcf89d98d8f990d4f920ab59441.
    
    commit af14a2a245af3a0be3e10e10d4f6277e492445e0
    Author: MrBaNkS <banksmi09@gmail.com>
    Date:   Sat Jul 19 17:53:52 2014 -0500
    
        Revert "Centered mobile settings icons"
    
        This reverts commit 3855197f0d94175a0b254b6f110646844b7e5f12.
    
    commit 7b46890a2087164708d4012f050fede67cd38fae
    Author: MrBaNkS <banksmi09@gmail.com>
    Date:   Sat Jul 19 17:53:34 2014 -0500
    
        Revert "Move Themes below Location"
    
        This reverts commit a201349765c79619aa7c2e7b9cf22ba3a5dddda8.

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -82,9 +82,7 @@ import com.android.settings.inputmethod.InputMethodAndLanguageSettings;
 import com.android.settings.inputmethod.KeyboardLayoutPickerFragment;
 import com.android.settings.inputmethod.SpellCheckersSettings;
 import com.android.settings.inputmethod.UserDictionaryList;
-import com.android.settings.location.LocationEnabler;
 import com.android.settings.location.LocationSettings;
-import com.android.settings.net.MobileDataEnabler;
 import com.android.settings.nfc.AndroidBeam;
 import com.android.settings.nfc.PaymentSettings;
 import com.android.settings.print.PrintJobSettingsFragment;
@@ -563,11 +561,6 @@ public class Settings extends PreferenceActivity
                 if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
                     target.remove(i);
                 }
-            } else if (id == R.id.mobile_network_settings) {
-                // Remove mobile network settings if the device doesn't have telephony
-                if (Utils.isWifiOnly(this)) {
-                    target.remove(i);
-                }
             } else if (id == R.id.data_usage_settings) {
                 // Remove data usage when kernel module not enabled
                 final INetworkManagementService netManager = INetworkManagementService.Stub
@@ -830,8 +823,6 @@ public class Settings extends PreferenceActivity
 
         private final WifiEnabler mWifiEnabler;
         private final BluetoothEnabler mBluetoothEnabler;
-        private final MobileDataEnabler mMobileDataEnabler;
-        private final LocationEnabler mLocationEnabler;
         private AuthenticatorHelper mAuthHelper;
         private DevicePolicyManager mDevicePolicyManager;
 
@@ -849,10 +840,7 @@ public class Settings extends PreferenceActivity
         static int getHeaderType(Header header) {
             if (header.fragment == null && header.intent == null) {
                 return HEADER_TYPE_CATEGORY;
-            } else if (header.id == R.id.wifi_settings
-                    || header.id == R.id.bluetooth_settings
-                    || header.id == R.id.mobile_network_settings
-                    || header.id == R.id.location_settings) {
+            } else if (header.id == R.id.wifi_settings || header.id == R.id.bluetooth_settings) {
                 return HEADER_TYPE_SWITCH;
             } else if (header.id == R.id.security_settings) {
                 return HEADER_TYPE_BUTTON;
@@ -898,8 +886,6 @@ public class Settings extends PreferenceActivity
             // Switches inflated from their layouts. Must be done before adapter is set in super
             mWifiEnabler = new WifiEnabler(context, new Switch(context));
             mBluetoothEnabler = new BluetoothEnabler(context, new Switch(context));
-            mMobileDataEnabler = new MobileDataEnabler(context, new Switch(context));
-            mLocationEnabler = new LocationEnabler(context, new Switch(context));
             mDevicePolicyManager = dpm;
         }
 
@@ -910,7 +896,7 @@ public class Settings extends PreferenceActivity
             int headerType = getHeaderType(header);
             View view = null;
 
-            if (convertView == null || headerType == HEADER_TYPE_SWITCH) {
+            if (convertView == null) {
                 holder = new HeaderViewHolder();
                 switch (headerType) {
                     case HEADER_TYPE_CATEGORY:
@@ -969,12 +955,8 @@ public class Settings extends PreferenceActivity
                     // Would need a different treatment if the main menu had more switches
                     if (header.id == R.id.wifi_settings) {
                         mWifiEnabler.setSwitch(holder.switch_);
-                    } else if (header.id == R.id.bluetooth_settings) {
+                    } else {
                         mBluetoothEnabler.setSwitch(holder.switch_);
-                    } else if (header.id == R.id.mobile_network_settings) {
-                        mMobileDataEnabler.setSwitch(holder.switch_);
-                    } else if (header.id == R.id.location_settings) {
-                        mLocationEnabler.setSwitch(holder.switch_);
                     }
                     updateCommonHeaderView(header, holder);
                     break;
@@ -1048,15 +1030,11 @@ public class Settings extends PreferenceActivity
         public void resume() {
             mWifiEnabler.resume();
             mBluetoothEnabler.resume();
-            mMobileDataEnabler.resume();
-            mLocationEnabler.resume();
         }
 
         public void pause() {
             mWifiEnabler.pause();
             mBluetoothEnabler.pause();
-            mMobileDataEnabler.pause();
-            mLocationEnabler.pause();
         }
     }
 
